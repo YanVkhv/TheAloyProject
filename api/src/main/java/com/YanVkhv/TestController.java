b140baa917b619e7b1179bfd611d7cb7bfb858e7
YanVkhv <yan.vkhv@gmail.com>
2020-02-06 20:40:29 +0100

    Initial commit

---
+++ api/src/main/java/com/YanVkhv/TestController.java
@@ -0,0 +1,12 @@
+package com.YanVkhv;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/")
+    public String index() {
+        return "This is where The Aloy Project starts!";
+    }
+}
