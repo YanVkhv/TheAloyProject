1a4c4a27f55c1d563bacb090435ece1a9728374f
YanVkhv <yan.vkhv@gmail.com>
2020-02-06 22:39:24 +0100

    Added Lombok plugin and made subscriber, subscription and base entity domain objects

---
+++ domain/src/main/java/com/YanVkhv/domain/Repository.java
@@ -0,0 +1,32 @@
+package com.YanVkhv.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class Repository<T extends BaseEntity> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public T save(T entity) {
+        entity.generateId();
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    public T update(T entity) {
+        entityManager.merge(entity);
+        return entity;
+    }
+
+    public abstract List<T> getAll();
+
+    public abstract T get(UUID entityId);
+
+}
---
+++ infrastructure/src/main/java/com/YanVkhv/infrastructure/builder/Builder.java
@@ -0,0 +1,7 @@
+package com.YanVkhv.infrastructure.builder;
+
+public abstract class Builder<T> {
+
+    public abstract T build();
+
+}
